373d4dc36499d6b52c99633ed9762093ff04d4c3
yernar <yernar.baiginzheyev@gmail.com>
2024-04-23 20:41:24 +0500

    Initial Commit

---
+++ FeedInstagram/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  FeedInstagram
+//
+//  Created by Yernar Baiginzheyev on 23.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
